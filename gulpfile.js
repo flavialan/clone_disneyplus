e886a55bcd896043d8ca683ad2a336ebce346298
Flavia Lopes <lalmeida.flavia@gmail.com>
2023-09-28 14:10:24 -0400

    Criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles(){
     return gulp.src('./src/styles/*.scss') //Funcao que recupera os arquivos.
@@ -7,7 +8,15 @@ function styles(){
         .pipe(gulp.dest('./dist/css')); //Envia os arquivos gerados para o destino escolhido
 }
 
-exports.default = styles;
+function images(){
+    return gulp.src('./src/images/**/*') //Funcao que recupera os arquivos.
+        .pipe(imagemin()) //Compila o sass e retorna arquivos comprimidos
+        .pipe(gulp.dest('./dist/images')); //Envia os arquivos gerados para o destino escolhido
+}
+
+exports.default = gulp.parallel(styles, images);
+
+
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) //Coloca os arquivos que serao observados e no array as funcoes que serao executadas
 }
