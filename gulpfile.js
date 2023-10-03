23c62436b0ca3437581ae709ac5eea7481c06b1d
Flavia Lopes <lalmeida.flavia@gmail.com>
2023-10-03 12:28:20 -0400

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts (){
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 function styles(){
     return gulp.src('./src/styles/*.scss') //Funcao que recupera os arquivos.
@@ -14,9 +21,10 @@ function images(){
         .pipe(gulp.dest('./dist/images')); //Envia os arquivos gerados para o destino escolhido
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) //Coloca os arquivos que serao observados e no array as funcoes que serao executadas
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
